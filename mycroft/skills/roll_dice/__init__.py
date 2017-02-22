58019bb451572cebccfbbe63e011cee5e5b372af
jarbasAI <somom@30wave.com>
2017-02-21 19:40:57 -0600

    dice roll skill

---
+++ mycroft/skills/roll_dice/__init__.py
@@ -0,0 +1,38 @@
+from os.path import dirname
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+__author__ = 'paul'
+
+LOGGER = getLogger(__name__)
+
+class RollDiceSkill(MycroftSkill):
+
+    def __init__(self):
+        super(RollDiceSkill, self).__init__(name="RollDiceSkill")
+
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+
+        roll_a_die_intent = IntentBuilder("RollADieIntent").\
+            require("RollADieKeyword").build()
+        self.register_intent(roll_a_die_intent, self.handle_roll_a_die_intent)
+
+        roll_two_dice_intent = IntentBuilder("RollTwoDiceIntent").\
+            require("RollTwoDiceKeyword").build()
+        self.register_intent(roll_two_dice_intent, self.handle_roll_two_dice_intent)
+
+    def handle_roll_a_die_intent(self, message):
+        self.speak_dialog("roll.a.die")
+
+    def handle_roll_two_dice_intent(self, message):
+        self.speak_dialog("roll.two.dice")
+
+    def stop(self):
+        pass
+
+
+def create_skill():
+    return RollDiceSkill()
