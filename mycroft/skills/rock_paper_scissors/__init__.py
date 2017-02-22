35a1743bb3a1617097f64e50926910da49a8518a
jarbasAI <somom@30wave.com>
2017-02-21 19:38:20 -0600

    rock paper scissors skill

---
+++ mycroft/skills/rock_paper_scissors/__init__.py
@@ -0,0 +1,31 @@
+from os.path import dirname
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+__author__ = 'paul'
+
+LOGGER = getLogger(__name__)
+
+class RockPaperScissorsSkill(MycroftSkill):
+
+    def __init__(self):
+        super(RockPaperScissorsSkill, self).__init__(name="RockPaperScissorsSkill")
+
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+
+        rock_paper_scissors_intent = IntentBuilder("RockPaperScissorsIntent").\
+            require("RockPaperScissorsKeyword").build()
+        self.register_intent(rock_paper_scissors_intent, self.handle_rock_paper_scissors_intent) 
+
+    def handle_rock_paper_scissors_intent(self, message):
+        self.speak_dialog("rock.paper.scissors")
+
+    def stop(self):
+        pass
+
+
+def create_skill():
+    return RockPaperScissorsSkill()
