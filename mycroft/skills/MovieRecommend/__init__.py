3841fb20cfb1d8c60d37066c52fcf806208ffbde
jarbasAI <somom@30wave.com>
2017-03-08 22:25:37 -0600

    added year info

--- mycroft/skills/MovieRecommend/__init__.py
+++ mycroft/skills/MovieRecommend/__init__.py
@@ -17,26 +17,32 @@ class MovieSkill(MycroftSkill):
 
     def __init__(self):
         super(MovieSkill, self).__init__(name="MovieSkill")
+        self.populate = True
+        self.reload_skill = False
+        # current movie info
         self.Movie = ""
         self.Synopsis = ""
         self.Director = ""
         self.Stars = ""
+        self.Year = ""
         self.imdblink = ""
-        self.movielist = []
-        self.imdbvocab = []
-        #### top 250 imdb movies
+        # top 250 imdb movies
         self.link = "http://www.imdb.com/chart/top?ref_=ft_250"
-        #### genres movie ####
+        self.movielist = []  # links to top movies
+        # movies genres
         self.genres = ["action", "adventure", "animation", "biography", "comedy", "crime","drama","family","fantasy",
                        "film_noir", "history", "horror", "music", "musical", "mystery", "romance", "sci_fi", "sport",
                        "thriller", "war", "western"]
         self.genreslink = "http://www.imdb.com/search/title?genres="
         self.genreslinkparams = "&sort=user_rating,desc&title_type=feature&num_votes=25000,&pf_rd_m=A2FGELUUNOQJNL"
-        self.genre_movie_list={}
+        self.genre_movie_list={}# links to top 50 movies of each genre
         for genre in self.genres:
             self.genre_movie_list.setdefault(genre, [])
-            #self.get_genre_movie_list(genre)
-        #self.get_movie_list()
+        # populate movie db
+        if self.populate:
+            self.get_movie_list()
+            for genre in self.genres:
+                self.get_genre_movie_list(genre)
 
     def initialize(self):
 
@@ -73,21 +79,21 @@ class MovieSkill(MycroftSkill):
 
         self.speak_dialog("movie_recommend", {"movie":self.Movie})
         self.speak_dialog("director", {"director":self.Director})
+        self.speak_dialog("year", {"year": self.Year})
         self.speak_dialog("stars", {"stars": self.Stars})
         self.speak_dialog("story", {"synopsis":self.Synopsis})
-
-       # self.add_result("Movie_Name",self.Movie)
-       # self.add_result("Movie_Director",self.Director)
-       # self.add_result("Movie_Stars", self.Stars)
-       # self.add_result("Movie_Synopsis", self.Synopsis)
-       # self.add_result("IMDB_Link", self.imdblink)
-       # self.emit_results()
 
     def handle_genre_suggest_intent(self, message):
 
         genre = message.data["genre"]
+        genre = genre.replace(" film", "")
+        genre = genre.replace(" movie", "")
+        genre = genre.replace(" ", "_")
+
         if len(self.genre_movie_list[genre])==0:
-            self.get_genre_movie_list(genre)
+            if not self.get_genre_movie_list(genre):
+                self.speak_dialog("invalidgenre")
+                return
 
         self.imdblink = random.choice(self.genre_movie_list[genre])
 
@@ -95,6 +101,7 @@ class MovieSkill(MycroftSkill):
 
         self.speak_dialog("movie_recommend", {"movie":self.Movie})
         self.speak_dialog("director", {"director":self.Director})
+        self.speak_dialog("year", {"year": self.Year})
         self.speak_dialog("stars", {"stars": self.Stars})
         self.speak_dialog("story", {"synopsis":self.Synopsis})
 
@@ -140,9 +147,9 @@ class MovieSkill(MycroftSkill):
                         self.genre_movie_list[genre].append(url) #add to link list
                 except:
                     pass
+            return True
         else:
-            self.speak("i dont know that movie genre")
-
+            return False
 
     def parse_movie(self, movie_link):
         response = requests.get(movie_link)
@@ -153,12 +160,13 @@ class MovieSkill(MycroftSkill):
         except:  #
             director = tree.xpath(".//*[@id='title-overview-widget']/div[3]/div[2]/div[1]/div[2]/span/a/span/text()")[0]
         story = tree.xpath(".//*[@id='titleStoryLine']/div[1]/p/text()")[0]
-        stars = \
-        tree.xpath(".//*[@id='title-overview-widget']/div[2]/div[2]/div/div[1]/div[1]/div[1]/strong/span/text()")[0]
+        stars = tree.xpath(".//*[@id='title-overview-widget']/div[2]/div[2]/div/div[1]/div[1]/div[1]/strong/span/text()")[0]
+        year = tree.xpath(".//*[@id='titleYear']/a/text()")[0]
         self.Movie = name
         self.Director = director
         self.Synopsis = story
         self.Stars = stars
+        self.Year = year
 
     def stop(self):
         pass
