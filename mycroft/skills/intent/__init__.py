5d3f31da543943e49fbd64f00b8e8f2be7b2d3f0
JarbasAI <somom@30wave.com>
2017-03-30 17:49:10 +0100

    Update __init__.py

--- mycroft/skills/intent/__init__.py
+++ mycroft/skills/intent/__init__.py
@@ -15,10 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
 
-from mycroft.skills.skill_intents import SkillIntents
+from mycroft.skills.intent_parser import IntentParser
 
 from mycroft.messagebus.message import Message
-from mycroft.skills.core import open_intent_envelope, MycroftSkill
+from mycroft.skills.core import  MycroftSkill
 from mycroft.util.log import getLogger
 
 import time
@@ -32,22 +32,21 @@ logger = getLogger(__name__)
 class IntentSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self, name="IntentSkill")
-        #self.engine = IntentDeterminationEngine()
-        self.intents = None
+        self.intent_parser = None
         self.reload_skill = False
         self.active_skills = [] # [skill_id , timestamp]
         self.intent_to_skill_id = {} # intent:source_skill_id
         self.converse_timeout = 5 # minutes to prune active_skills
 
     def initialize(self):
-        self.intents = SkillIntents(self.emitter)
+        self.intent_parser = IntentParser(self.emitter)
         self.emitter.on('register_intent', self.handle_register_intent)
         self.emitter.on('recognizer_loop:utterance', self.handle_utterance)
         self.emitter.on('converse_status_response', self.handle_conversation_response)
 
-    def do_conversation(self, utterances, skill):
+    def do_conversation(self, utterances, skill_id, lang):
         self.emitter.emit(Message("converse_status_request", {
-                          "skill_id": skill, "utterances": utterances}))
+                          "skill_id": skill_id, "utterances": utterances, "lang":lang}))
         self.waiting = True
         self.result = False
         while self.waiting:
@@ -74,6 +73,10 @@ class IntentSkill(MycroftSkill):
         self.active_skills.insert(0, [skill_id, time.time()])
 
     def handle_utterance(self, message):
+        # Get language of the utterance
+        lang = message.data.get('lang', None)
+        if not lang:
+            lang = "en-us"
 
         utterances = message.data.get('utterances', '')
         # check for conversation time-out
@@ -82,9 +85,9 @@ class IntentSkill(MycroftSkill):
 
         # check if any skill wants to handle utterance
         for skill in self.active_skills:
-            if self.do_conversation(utterances, skill[0]):
+            if self.do_conversation(utterances, skill[0], lang):
                 # update timestamp, or there will be a timeout where
-                # intent stops conversing wether its being used or not
+                # intent stops conversing whether its being used or not
                 self.add_active_skill(skill[0])
                 return
         # no skill wants to handle utterance, proceed
@@ -93,28 +96,34 @@ class IntentSkill(MycroftSkill):
         success = False
 
         try:
-            success, best_intent = self.intents.determine_intent(utterances)
+            success, best_intent = self.intent_parser.determine_intent(utterances)
         except:
             print "FAIL"
 
         if success:
-            self.intents.execute_intent()
+            self.intent_parser.execute_intent()
+
             # best intent detected -> update called skills dict
             skill_id = self.intent_to_skill_id[best_intent['intent_type']]
             self.add_active_skill(skill_id)
+            # process feedback
+            if best_intent['intent_type'] == "PositiveFeedbackIntent" or best_intent['intent_type'] == "NegativeFeedbackIntent":
+                self.emitter.emit(Message("feedback_id",{"active_skill":self.active_skills[1][0]}))
 
         elif len(utterances) == 1:
             self.emitter.emit(Message("intent_failure", {
-                "utterance": utterances[0]
+                "utterance": utterances[0],
+                "lang": lang
             }))
         else:
             self.emitter.emit(Message("multi_utterance_intent_failure", {
-                "utterances": utterances
+                "utterances": utterances,
+                "lang": lang
             }))
 
     def handle_register_intent(self, message):
         intent = message.data["intent"]
-        self.intents.register_intent(intent)
+        self.intent_parser.register_intent(intent)
         # map intent to source skill
         self.intent_to_skill_id.setdefault(
             intent["name"], message.data["source_skill"])
