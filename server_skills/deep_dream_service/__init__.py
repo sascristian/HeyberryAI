8cd2e88592e451111d1bb5cea3ff68c030f92a4c
jarbasai <jarbasai>
2017-06-05 17:16:10 +0100

    dream service

--- server_skills/deep_dream_service/__init__.py
+++ server_skills/deep_dream_service/__init__.py
@@ -13,166 +13,169 @@ from mycroft.skills.core import MycroftSkill
 
 __author__ = 'jarbas'
 
+
 class DreamService(MycroftSkill):
-	def __init__(self):
-		super(DreamService, self).__init__(name="DreamSkill")
-		self.reload_skill = False
 
-		caffepath = "home/test/caffe"#self.config["caffe_path"]
+    def __init__(self):
+        super(DreamService, self).__init__(name="DreamSkill")
+        self.reload_skill = False
 
-		sys.path.append(caffepath)
-		from batcountry import BatCountry
+        caffepath = "home/test/caffe"#self.config["caffe_path"]
 
-		path = caffepath
-		self.model = "bvlc_googlenet"
-		path += '/models/' + self.model
+        sys.path.append(caffepath)
+        from batcountry import BatCountry
 
-		# start batcountry instance (self, base_path, deploy_path=None, model_path=None,
-		self.bc = BatCountry(path)#path,model_path=path)
-		#if model == "bvlc_googlenet":
-		self.iter = 20#self.config["iter"] #dreaming iterations
-		self.layers = [ "inception_5b/output", "inception_5b/pool_proj",
-						"inception_5b/pool", "inception_5b/5x5",
-						"inception_5b/5x5_reduce", "inception_5b/3x3",
-						"inception_5b/3x3_reduce", "inception_5b/1x1",
-						"inception_5a/output", "inception_5a/pool_proj",
-						"inception_5a/pool", "inception_5a/5x5",
-						"inception_5a/5x5_reduce", "inception_5a/3x3",
-						"inception_5a/3x3_reduce", "inception_5a/1x1",
-						"pool4/3x3_s2", "inception_4e/output", "inception_4e/pool_proj",
-						"inception_4e/pool", "inception_4e/5x5",
-						"inception_4e/5x5_reduce", "inception_4e/3x3",
-						"inception_4e/3x3_reduce", "inception_4e/1x1",
-						"inception_4d/output", "inception_4d/pool_proj",
-						"inception_4d/pool", "inception_4d/5x5",
-						"inception_4d/5x5_reduce", "inception_4d/3x3",
-						"inception_4d/3x3_reduce", "inception_4d/1x1",
-						"inception_4c/output", "inception_4c/pool_proj",
-						"inception_4c/pool", "inception_4c/5x5",
-						"inception_4c/5x5_reduce", "inception_4c/3x3",
-						"inception_4c/3x3_reduce", "inception_4c/1x1",
-						"inception_4b/output", "inception_4b/pool_proj",
-						"inception_4b/pool", "inception_4b/5x5",
-						"inception_4b/5x5_reduce", "inception_4b/3x3",
-						"inception_4b/3x3_reduce", "inception_4b/1x1",
-						"inception_4a/output", "inception_4a/pool_proj",
-						"inception_4a/pool", "inception_4a/5x5",
-						"inception_4a/5x5_reduce", "inception_4a/3x3",
-						"inception_4a/3x3_reduce", "inception_4a/1x1",
-						"inception_3b/output", "inception_3b/pool_proj",
-						"inception_3b/pool", "inception_3b/5x5",
-						"inception_3b/5x5_reduce", "inception_3b/3x3",
-						"inception_3b/3x3_reduce", "inception_3b/1x1",
-						"inception_3a/output", "inception_3a/pool_proj",
-						"inception_3a/pool", "inception_3a/5x5",
-						"inception_3a/5x5_reduce", "inception_3a/3x3",
-						"inception_3a/3x3_reduce", "inception_3a/1x1",
-						"pool2/3x3_s2","conv2/norm2","conv2/3x3",
-						"conv2/3x3_reduce", "pool1/norm1"] #"pool1/3x3_s2" , "conv17x7_s2"
+        path = caffepath
+        self.model = "bvlc_googlenet"
+        path += '/models/' + self.model
 
-		###imagine dimensions
-		self.w = 640
-		self.h = 480
+        # start batcountry instance (self, base_path, deploy_path=None, model_path=None,
+        self.bc = BatCountry(path)#path,model_path=path)
+        #if model == "bvlc_googlenet":
+        self.iter = 20#self.config["iter"] #dreaming iterations
+        self.layers = [ "inception_5b/output", "inception_5b/pool_proj",
+                        "inception_5b/pool", "inception_5b/5x5",
+                        "inception_5b/5x5_reduce", "inception_5b/3x3",
+                        "inception_5b/3x3_reduce", "inception_5b/1x1",
+                        "inception_5a/output", "inception_5a/pool_proj",
+                        "inception_5a/pool", "inception_5a/5x5",
+                        "inception_5a/5x5_reduce", "inception_5a/3x3",
+                        "inception_5a/3x3_reduce", "inception_5a/1x1",
+                        "pool4/3x3_s2", "inception_4e/output", "inception_4e/pool_proj",
+                        "inception_4e/pool", "inception_4e/5x5",
+                        "inception_4e/5x5_reduce", "inception_4e/3x3",
+                        "inception_4e/3x3_reduce", "inception_4e/1x1",
+                        "inception_4d/output", "inception_4d/pool_proj",
+                        "inception_4d/pool", "inception_4d/5x5",
+                        "inception_4d/5x5_reduce", "inception_4d/3x3",
+                        "inception_4d/3x3_reduce", "inception_4d/1x1",
+                        "inception_4c/output", "inception_4c/pool_proj",
+                        "inception_4c/pool", "inception_4c/5x5",
+                        "inception_4c/5x5_reduce", "inception_4c/3x3",
+                        "inception_4c/3x3_reduce", "inception_4c/1x1",
+                        "inception_4b/output", "inception_4b/pool_proj",
+                        "inception_4b/pool", "inception_4b/5x5",
+                        "inception_4b/5x5_reduce", "inception_4b/3x3",
+                        "inception_4b/3x3_reduce", "inception_4b/1x1",
+                        "inception_4a/output", "inception_4a/pool_proj",
+                        "inception_4a/pool", "inception_4a/5x5",
+                        "inception_4a/5x5_reduce", "inception_4a/3x3",
+                        "inception_4a/3x3_reduce", "inception_4a/1x1",
+                        "inception_3b/output", "inception_3b/pool_proj",
+                        "inception_3b/pool", "inception_3b/5x5",
+                        "inception_3b/5x5_reduce", "inception_3b/3x3",
+                        "inception_3b/3x3_reduce", "inception_3b/1x1",
+                        "inception_3a/output", "inception_3a/pool_proj",
+                        "inception_3a/pool", "inception_3a/5x5",
+                        "inception_3a/5x5_reduce", "inception_3a/3x3",
+                        "inception_3a/3x3_reduce", "inception_3a/1x1",
+                        "pool2/3x3_s2","conv2/norm2","conv2/3x3",
+                        "conv2/3x3_reduce", "pool1/norm1"] #"pool1/3x3_s2" , "conv17x7_s2"
 
-		### flag to avoid dreaming multiple times at once
-		self.dreaming = False
+        ###imagine dimensions
+        self.w = 640
+        self.h = 480
 
-		self.outputdir = self.config_core["database_path"] + "/dreams/"
+        ### flag to avoid dreaming multiple times at once
+        self.dreaming = False
 
-	def initialize(self):
-		self.emitter.on("deep_dream_request", self.handle_dream)
+        self.outputdir = self.config_core["database_path"] + "/dreams/"
 
-		dream_status_intent = IntentBuilder("DreamIntent") \
-			.require("dream").build()
-		self.register_intent(dream_status_intent,
-							 self.handle_dream_status_intent)
+    def initialize(self):
+        self.emitter.on("deep_dream_request", self.handle_dream)
 
-	def handle_dream_status_intent(self, message):
-		self.speak("deep dream service loaded")
+        dream_status_intent = IntentBuilder("DreamIntent") \
+            .require("dream").build()
+        self.register_intent(dream_status_intent,
+                             self.handle_dream_status_intent)
 
-	def handle_dream(self, message):
-		source = message.data.get("dream_source")
-		guide = message.data.get("dream_guide")
-		name = message.data.get("dream_name")
+    def handle_dream_status_intent(self, message):
+        self.speak("deep dream service loaded")
 
-		if name is None:
-			name = time.asctime()
+    def handle_dream(self, message):
+        source = message.data.get("dream_source")
+        guide = message.data.get("dream_guide")
+        name = message.data.get("dream_name")
 
-		if guide is not None:
-			result = self.guided_dream(source, guide, name)
-		else:
-			result = self.dream(source, name)
+        if name is None:
+            name = time.asctime()
 
-		if result is not None:
-			# TODO upload on flickr, send remote link
-			self.speak("Here is what i dreamed", metadata={"dream_url":result})
+        if guide is not None:
+            result = self.guided_dream(source, guide, name)
+        else:
+            result = self.dream(source, name)
 
-	#### dreaming functions
-	def dream(self, imagepah, name):
-		fails = 0
-		if self.dreaming:
-			self.speak("i am dreaming")
-			return None
-		else:
-			self.speak_dialog("dream")
-			self.speak("please wait while the dream is processed")
+        if result is not None:
+            # TODO upload on flickr, send remote link
+            self.speak("Here is what i dreamed", metadata={"dream_url": result})
 
-		while fails <= 5:
-			layer = random.choice(self.layers)
-			try:
-				dreampic = imutils.resize(cv2.imread(imagepah), self.w, self.h)  # cv2.resize(img, (640, 480))
-				self.dreaming = True
-				image = self.bc.dream(np.float32(dreampic), end=layer, iter_n=int(self.iter))
-				# write the output image to file
-				result = Image.fromarray(np.uint8(image))
-				outpath = self.outputdir + "/" + name
-				result.save(outpath)
-				self.dreaming = False
-				return outpath
-			except:
-				fails += 1
+    #### dreaming functions
+    def dream(self, imagepah, name):
+        fails = 0
+        if self.dreaming:
+            self.speak("i am dreaming")
+            return None
+        else:
+            self.speak_dialog("dream")
+            self.speak("please wait while the dream is processed", more=True)
 
-		self.dreaming = False
-		self.speak("Could not dream this time")
-		return None
+        while fails <= 5:
+            layer = random.choice(self.layers)
+            try:
+                dreampic = imutils.resize(cv2.imread(imagepah), self.w, self.h)  # cv2.resize(img, (640, 480))
+                self.dreaming = True
+                image = self.bc.dream(np.float32(dreampic), end=layer, iter_n=int(self.iter))
+                # write the output image to file
+                result = Image.fromarray(np.uint8(image))
+                outpath = self.outputdir + "/" + name
+                result.save(outpath)
+                self.dreaming = False
+                return outpath
+            except:
+                fails += 1
 
-	def guided_dream(self, sourcepath, guidepath, name):
-		# dreampic = np.zeros((480, 640, 3), np.uint8)
-		if self.dreaming:
-			self.speak("i am dreaming")
-			return None
-		else:
-			self.speak_dialog("dream")
-			self.speak("please wait while the dream is processed")
-		fails = 0
-		while fails <= 5:
-			self.dreaming = True
-			layer = random.choice(self.layers)
-			try:
-				features = self.bc.prepare_guide(Image.open(guidepath), end=layer)
-				dreampic = imutils.resize(cv2.imread(sourcepath), self.w, self.h)  # cv2.resize(img, (640, 480))
-				image = self.bc.dream(np.float32(dreampic), end=layer,
-									  iter_n=int(self.iter), objective_fn=self.bc.guided_objective,
-									  objective_features=features)
+        self.dreaming = False
+        self.speak("Could not dream this time")
+        return None
 
-				# write the output image to file
-				result = Image.fromarray(np.uint8(image))
-				outpath = self.outputdir + name
-				result.save(outpath)
-				self.dreaming = False
-				return outpath
-			except:
-				fails += 1
-		self.dreaming = False
-		self.speak("Could not dream this time")
-		return None
+    def guided_dream(self, sourcepath, guidepath, name):
+        # dreampic = np.zeros((480, 640, 3), np.uint8)
+        if self.dreaming:
+            self.speak("i am dreaming")
+            return None
+        else:
+            self.speak_dialog("dream")
+            self.speak("please wait while the dream is processed")
+        fails = 0
+        while fails <= 5:
+            self.dreaming = True
+            layer = random.choice(self.layers)
+            try:
+                features = self.bc.prepare_guide(Image.open(guidepath), end=layer)
+                dreampic = imutils.resize(cv2.imread(sourcepath), self.w, self.h)  # cv2.resize(img, (640, 480))
+                image = self.bc.dream(np.float32(dreampic), end=layer,
+                                      iter_n=int(self.iter), objective_fn=self.bc.guided_objective,
+                                      objective_features=features)
 
-	def stop(self):
-		try:
-			self.bc.cleanup()
-			cv2.destroyAllWindows()
-		except:
-			pass
+                # write the output image to file
+                result = Image.fromarray(np.uint8(image))
+                outpath = self.outputdir + name
+                result.save(outpath)
+                self.dreaming = False
+                return outpath
+            except:
+                fails += 1
+        self.dreaming = False
+        self.speak("Could not dream this time")
+        return None
+
+    def stop(self):
+        try:
+            self.bc.cleanup()
+            cv2.destroyAllWindows()
+        except:
+            pass
+
 
 def create_skill():
-	return DreamService()
+    return DreamService()
