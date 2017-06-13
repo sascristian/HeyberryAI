e9f179797cc7ce741da065bf5345d57928439e13
jarbasai <jarbasai>
2017-06-13 20:05:38 +0100

    facerecog

--- server_skills/face_recog_service/__init__.py
+++ server_skills/face_recog_service/__init__.py
@@ -22,6 +22,7 @@ class FaceRecService(MycroftSkill):
             # Get the face encodings for each face in each image file
             # Since there could be more than one face in each image, it returns a list of encordings.
             # But since i assume each image only has one face, I only care about the first encoding in each image, so I grab index 0.
+            self.log.info("loading face encodings for " + face)
             self.known_faces[face] = face_recognition.face_encodings(face_recognition.load_image_file(os.path.dirname(__file__) + "/known faces/" + face))[0]
 
     def initialize(self):
@@ -30,30 +31,30 @@ class FaceRecService(MycroftSkill):
     def handle_recog(self, message):
         face = message.data.get("file")
         user_id = message.data.get("source")
-
+        self.log.info(user_id + " request facerecog for " + face)
         if user_id is not None:
             if user_id == "unknown":
                 user_id = "all"
-            self.target = user_id
+            #self.target = user_id
         else:
             self.log.warning("no user/target specified")
             user_id = "all"
 
         result = None
         # read unknown image
+        self.log.info("loading unknown image")
         unknown_image = face_recognition.load_image_file(face)
         # results is an array of True/False telling if the unknown face matched anyone in the known_faces array
         for person in self.known_faces:
+            self.log.info("comparing to person " + person)
             # check if unknown person is this face, by comparing face encodings
             if face_recognition.compare_faces(self.known_faces[person], face_recognition.face_encodings(unknown_image)[0]):
                 result = person.replace(".jpg", "")
+                self.log.info("match found, unknown image is " + result)
                 break
 
-        print result
-        if result is not None:
-            self.speak(result)
-        else:
-            self.speak("i dont know this person")
+        self.speak(result)
+
         self.emitter.emit(Message("message_request",
                                   {"user_id": user_id, "data": {"result": result}, "type": "face_recognition_result"}))
 
