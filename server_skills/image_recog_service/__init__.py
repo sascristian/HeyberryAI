089fed6285885f710f6f453a541cda9f0b796172
jarbasai <jarbasai>
2017-06-17 17:24:08 +0100

    testing

--- server_skills/image_recog_service/__init__.py
+++ server_skills/image_recog_service/__init__.py
@@ -1,17 +1,9 @@
-import cv2
 import numpy as np
-import time
-import random
-from PIL import Image
-import imutils
 import sys
-import urllib
-import os
+from os.path import dirname
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.messagebus.message import Message
-
-from imgurpython import ImgurClient
 
 __author__ = 'jarbas'
 
@@ -21,23 +13,43 @@ class ImageRecognitionService(MycroftSkill):
     def __init__(self):
         super(ImageRecognitionService, self).__init__(name="ImageRecognitionSkill")
         self.reload_skill = False
-
+        # load caffe
         try:
-            path = self.config["caffe_path"]
+            self.path = self.config["caffe_path"]
         except:
-            path = "../caffe"
+            self.path = "../caffe"
 
-        sys.path.insert(0, path + '/python')
+        sys.path.insert(0, self.path + '/python')
         import caffe
+        # load net
+        try:
+           self.model = self.config["caffe_path"]
+        except:
+            self.model = "bvlc_googlenet"
 
-        self.model = "bvlc_googlenet"
-        path += '/models/' + self.model
+        path = self.path + '/models/' + self.model
 
         self.net = caffe.Net(path + '/deploy.prototxt',
                         path + '/'+self.model+'.caffemodel', caffe.TEST)
 
+        # pre-process input image stuff
+        self.transformer = caffe.io.Transformer({'data': self.net.blobs['data'].data.shape})
+        # set mean_image
+        self.transformer.set_mean('data',
+                             np.load(self.path + '/python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))
+        # subtract mean image from input image
+        self.transformer.set_transpose('data', (2, 0, 1))
+        self.transformer.set_channel_swap('data', (2, 1, 0))  # if using RGB instead of BGR
+        self.transformer.set_raw_scale('data', 255.0)
+        # reshape the blobs so that they match the image shape file
+        self.net.blobs['data'].reshape(1, 3, 227, 227)
+
+        # output to text
+        self.label_mapping = np.loadtxt(dirname(__file__) + "/synset_words.txt", str, delimiter='\t')
+
+
     def initialize(self):
-        self.emitter.on("image_classify_request", self.handle_classify)
+        self.emitter.on("image_classification_request", self.handle_classify)
 
         dream_status_intent = IntentBuilder("ImageClassfyStatusIntent") \
             .require("imgstatus").build()
@@ -46,12 +58,10 @@ class ImageRecognitionService(MycroftSkill):
 
     def handle_img_recog_intent(self, message):
         self.speak_dialog("imgrecogstatus")
+        self.emitter.emit(Message("image_classification_request", {"file":dirname(__file__)+"/obama.jpg", "source":"testing"}))
 
     def handle_classify(self, message):
-        # TODO dreaming queue
-        source = message.data.get("dream_source")
-        guide = message.data.get("dream_guide")
-        name = message.data.get("dream_name")
+        pic = message.data.get("file")
         user_id = message.data.get("source")
 
         if user_id is not None:
@@ -62,20 +72,29 @@ class ImageRecognitionService(MycroftSkill):
             self.log.warning("no user/target specified")
             user_id = "all"
 
-        if name is None:
-            name = time.asctime().replace(" ","_") + ".jpg"
+        # load
+        img = caffe.io.load_image(pic)
+        self.net.blobs['data'].data[...] = self.transformer.preprocess('data', img)
+        # get result
+        output = self.net.forward()
+        print output['prob'].argmax()
+        best_n = self.net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
+        print self.label_mapping[best_n]
 
-        if guide is not None:
-            result = self.guided_dream(source, guide, name)
-        else:
-            result = self.dream(source, name)
-
-        print result
-        if result is not None:
-            data = self.client.upload_from_path(result)
-            link = data["link"]
-            self.speak("Here is what i dreamed", metadata={"dream_url": link})
-            self.emitter.emit(Message("message_request", {"user_id":user_id, "data":{"dream_url":link}, "type":"deep_dream_result"}))
+        # send result
+        msg_type = "image_classification_result"
+        msg_data = {"classfication":self.label_mapping[best_n]}
+        # to source socket
+        try:
+            if user_id.split(":")[1].isdigit():
+                self.emitter.emit(Message("message_request",
+                                          {"user_id": user_id, "data":msg_data,
+                                           "type": msg_type}))
+        except:
+            pass
+        # to bus
+        self.emitter.emit(Message(msg_type,
+                                  msg_data))
 
     def stop(self):
         pass
