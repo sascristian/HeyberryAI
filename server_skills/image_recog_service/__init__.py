9e2ce012e03dbbbffc84ce369a2d993742f2a8e1
jarbasai <jarbasai>
2017-06-17 20:24:21 +0100

    transformer

--- server_skills/image_recog_service/__init__.py
+++ server_skills/image_recog_service/__init__.py
@@ -1,5 +1,5 @@
 import numpy as np
-import sys, cv2
+import sys
 from os.path import dirname
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
@@ -36,17 +36,6 @@ class ImageRecognitionService(MycroftSkill):
                                     channel_swap=(2, 1, 0),
                                     raw_scale=255,
                                     image_dims=(224, 224))
-
-        # pre-process input image stuff
-        #self.transformer = caffe.io.Transformer({'data': self.net.blobs['data'].data.shape})
-        # set mean_image
-        #self.transformer.set_mean('data', np.load().mean(1).mean(1))
-        # subtract mean image from input image
-        #self.transformer.set_transpose('data', (2, 0, 1))
-        #self.transformer.set_channel_swap('data', (2, 1, 0))  # if using RGB instead of BGR
-        #self.transformer.set_raw_scale('data', 255.0)
-        # reshape the blobs so that they match the image shape file
-        #self.net.blobs['data'].reshape(1, 3, 227, 227)
 
         # output to text
         self.label_mapping = np.loadtxt(dirname(__file__) + "/synset_words.txt", str, delimiter='\t')
