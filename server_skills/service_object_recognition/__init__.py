b368cd4f3ffc8cda10af185c8015a00f10e7e772
jarbasai <jarbasai>
2017-07-05 23:04:03 +0100

    fix imports

--- server_skills/service_object_recognition/__init__.py
+++ server_skills/service_object_recognition/__init__.py
@@ -27,7 +27,7 @@ import numpy as np
 import tensorflow as tf
 from os.path import dirname
 
-sys.path.append('/opt/mycroft/skills/skill-realtime-object-recognition')
+sys.path.append(dirname(__file__))
 
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
