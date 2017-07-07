a5f65c7f0ea3c4f294641acd0ac77c8cbeab0fb5
jarbasai <jarbasai>
2017-07-07 02:50:30 +0100

    update

--- mycroft/client/server/main.py
+++ mycroft/client/server/main.py
@@ -38,7 +38,7 @@ blacklisted_ips = []
 whitelisted_ips = []
 blacklist = True
 
-allowed_bus_messages = ["recognizer_loop:utterance", "names_response", "id_update", "incoming_file", "vision_result", "vision.faces.result", "vision.feed.result", "image.classification.request", "class.visualization.request", "face_recognition_request"]
+allowed_bus_messages = ["recognizer_loop:utterance", "names_response", "id_update", "incoming_file", "vision_result", "vision.faces.result", "vision.feed.result", "image.classification.request", "class.visualization.request", "face.recognition.request"]
 names = {}#name, sock this name refers to
 users = {}#sock, [current user of sock]
 
@@ -298,18 +298,16 @@ def main():
                                 elif deserialized_message.type == "incoming_file":
                                     logger.info("started receiving file for " + str(sock_num))
                                     file_socks[sock_num] = open("../tmp_file.jpg", 'wb')
-                                # TODO merge all files operations
-                                elif deserialized_message.type == "face_recognition_request":
+
+                                # set temp file path
+                                if "file" in deserialized_message.data.keys():
                                     deserialized_message.data["file"] = "../tmp_file.jpg"
                                     ws.emit(Message(deserialized_message.type, deserialized_message.data, context))
-                                elif deserialized_message.type == "vision_result":
+                                elif "feed_path" in deserialized_message.data.keys():
                                     deserialized_message.data["feed_path"] = "../tmp_file.jpg"
                                     ws.emit(Message(deserialized_message.type, deserialized_message.data, context))
-                                elif deserialized_message.type == "image.classification.request":
-                                    deserialized_message.data["file"] = "../tmp_file.jpg"
-                                    ws.emit(Message(deserialized_message.type, deserialized_message.data, context))
-                                elif deserialized_message.type == "vision.feed.result":
-                                    deserialized_message.data["file"] = "../tmp_file.jpg"
+                                elif "path" in deserialized_message.data.keys():
+                                    deserialized_message.data["path"] = "../tmp_file.jpg"
                                     ws.emit(Message(deserialized_message.type, deserialized_message.data, context))
                                 else:
                                     logger.info("no special handling provided for " + deserialized_message.type)
--- server_skills/skill_user_id/__init__.py
+++ server_skills/skill_user_id/__init__.py
@@ -19,132 +19,17 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
-import time, urllib
-from mycroft.messagebus.message import Message
-from os.path import dirname
+from mycroft.util.services import FaceRecognitionService, VisionService
 
 __author__ = 'jarbas'
 
 logger = getLogger(__name__)
 
 
-class UserIdService():
-    def __init__(self, emitter, timeout=20, logger=None, server=False):
-        self.emitter = emitter
-        self.waiting = False
-        self.server = server
-        self.face_recog_result = None
-        self.vision_result = None
-        self.timeout = timeout
-        if logger is not None:
-            self.logger = logger
-        else:
-            self.logger = getLogger("User_ID")
-        self.emitter.on("vision_result", self.end_wait)
-        self.emitter.on("face.recognition.result", self.end_wait)
-
-    def end_wait(self, message):
-        # TODO implemnt all wait cases
-        if message.type == "face.recognition.result":
-            self.logger.info("face recognition result received")
-            self.face_recog_result = message.data["result"]
-        if message.type == "vision_result":
-            self.logger.info("vision result received")
-            self.vision_result = message.data
-        self.waiting = False
-
-    def wait(self):
-        start = time.time()
-        elapsed = 0
-        self.waiting = True
-        while self.waiting and elapsed < self.timeout:
-            elapsed = time.time() - start
-            time.sleep(0.1)
-
-    def local_face_recog(self, picture_path, user_id):
-        requester = user_id
-        message_type = "face.recognition.request"
-        context = {"destinatary": user_id}
-        message_data = {"file": picture_path, "source": requester, "user":"unknown"}
-        self.emitter.emit(Message(message_type, message_data, context))
-        self.wait()
-        result = self.face_recog_result
-        return result
-
-    def server_face_recog(self, picture_path, user_id):
-        requester = user_id
-        message_type = "face.recognition.request"
-        message_data = {"file": picture_path, "source": requester}
-        context = {"destinatary": user_id}
-        self.emitter.emit(Message("server_request", {"server_msg_type":"file", "requester":requester, "message_type": message_type, "message_data": message_data}, context))
-        self.wait()
-        result = self.face_recog_result
-        return result
-
-    def request_vision_id(self, user_id):
-        self.logger.info("Vision Recognition requested")
-        self.vision_result = None
-        self.face_recog_result = None
-        if self.server:
-            # TODO request vision result from client
-            # request vision service for feed
-            self.logger.info("Requesting client vision service")
-            self.emitter.emit(Message("message_request", {"user_id": user_id, "type": "vision_request", "data": {}}))
-            self.wait()
-            if self.vision_result is None:
-                self.logger.info("No vision result received for " + str(self.timeout) + " seconds, aborting")
-                return None
-            if self.vision_result["num_persons"] == 0:
-                self.logger.info("No persons detected")
-                return None
-            self.logger.info("Requesting local face recognition service")
-            return self.local_face_recog(self.vision_result["feed_path"], user_id)
-
-        else:
-            # request vision service for feed
-            self.logger.info("Requesting local vision service")
-            self.emitter.emit(Message("vision_request", {}))
-            self.wait()
-            if self.vision_result["num_persons"] == 0:
-                self.logger.info("No persons detected")
-                return None
-            self.logger.info("Requesting face recognition from server")
-            return self.server_face_recog(self.vision_result["feed_path"], user_id)
-
-    def face_recog_from_url(self, url, user_id):
-        self.logger.info("URL Face Recognition requested")
-        self.vision_result = None
-        self.face_recog_result = None
-        if self.server:
-            self.logger.info("Requesting local face recognition service")
-            saved_url = dirname(__file__)+"/temp.jpg"
-            f = open(saved_url, 'wb')
-            f.write(urllib.urlopen(url).read())
-            f.close()
-            return self.local_face_recog(saved_url, user_id)
-
-        else:
-            self.logger.info("Requesting face recognition from server")
-            saved_url = dirname(__file__) + "/temp.jpg"
-            f = open(saved_url, 'wb')
-            f.write(urllib.urlopen(url).read())
-            f.close()
-            return self.server_face_recog(saved_url, user_id)
-
-    def request_bluetooth_id(self, user_id):
-        self.logger.error("Bluetooth recognition requested but not implemented")
-        return None
-
-    def request_voice_print_id(self, user_id):
-        self.logger.error("Voice recognition requested but not implemented")
-        return None
-
-
 class UserIdSkill(MycroftSkill):
 
     def __init__(self):
         super(UserIdSkill, self).__init__(name="User Identification Skill")
-        self.server = True
         self.photo = None
 
     def initialize(self):
@@ -159,46 +44,49 @@ class UserIdSkill(MycroftSkill):
         self.register_intent(what_am_i_intent,
                              self.handle_what_am_i_intent)
 
-        self.userid = UserIdService(self.emitter, server=self.server)
-
     def handle_who_am_i_intent(self, message):
         user_id = message.context.get("destinatary")
         user = ""
+        face_recog = FaceRecognitionService(self.emitter)
         if "fbchat" in user_id:
             url = message.context.get("photo")
-            vision_user = self.userid.face_recog_from_url(url, user_id)
+            vision_user = face_recog.face_recognition_from_url(url, self.context, server=False)
         else:
-            vision_user = self.userid.request_vision_id(user_id)
+            vision = VisionService(self.emitter)
+            feed = vision.get_feed(self.context)
+            vision_user = face_recog.face_recognition_from_file(feed, self.context, server=False)
+
         if vision_user is None:
             vision_user = "unknown"
         if vision_user != "unknown":
             user += vision_user + ", according to vision service\n"
 
-        voice_user = self.userid.request_voice_print_id(user_id)
-        if voice_user is None:
-            voice_user = "unknown"
-        if voice_user != "unknown":
-            user += voice_user + ", according to voice print service\n"
+        #voice_user = self.userid.request_voice_print_id(user_id)
+        #if voice_user is None:
+        #    voice_user = "unknown"
+        #if voice_user != "unknown":
+        #    user += voice_user + ", according to voice print service\n"
 
-        bluetooth_user = self.userid.request_bluetooth_id(user_id)
-        if bluetooth_user is None:
-            bluetooth_user = "unknown"
-        if bluetooth_user != "unknown":
-            user += bluetooth_user + ", according to bluetooth service\n"
+        #bluetooth_user = self.userid.request_bluetooth_id(user_id)
+        #if bluetooth_user is None:
+        #    bluetooth_user = "unknown"
+        #if bluetooth_user != "unknown":
+        #    user += bluetooth_user + ", according to bluetooth service\n"
 
         usr = message.context.get("user", "unknown")
         if usr == "unknown":
             usr = "unknown " + message.context.get("destinatary") + " user"
         user += usr + ", according to source of message\n"
+
         self.speak_dialog("who.user.is", {"username": user})
 
     def handle_what_am_i_intent(self, message):
-        try:
+        if ":" in message.context.get("destinatary", ""):
             # server
             target, sock_num = message.context.get("destinatary").split(":")
-        except:
+        else:
             # non server
-            target = message.context.get("destinatary")
+            target = message.context.get("destinatary", "unknown source")
             self.log.debug("could not get socket from " + str(target))
         self.speak_dialog("what.user.is", {"usertype": target})
 
