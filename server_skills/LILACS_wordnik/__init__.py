4041e3706f72cf713420e5aeed5cb0c0864b300f
jarbasai <jarbasai>
2017-07-21 17:11:14 +0100

    fix api

--- server_skills/LILACS_wordnik/__init__.py
+++ server_skills/LILACS_wordnik/__init__.py
@@ -33,7 +33,9 @@ class LILACSWordnikSkill(MycroftSkill):
             name="LILACS_Wordnik_Skill")
         apiUrl = 'http://api.wordnik.com/v4'
         apiKey = self.config_core.get(
-            "APIS").get("Wordnik",  self.config.get("Wordnik"))
+            "APIS").get("Wordnik")
+        if not apiKey:
+            apiKey = self.config.get("Wordnik")
         client = swagger.ApiClient(apiKey, apiUrl)
         self.wordApi = WordApi.WordApi(client)
         self.limit = 5
