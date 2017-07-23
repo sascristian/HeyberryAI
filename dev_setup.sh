9c96c411ebfcbbe211e0c41aa57a9251c7dd5477
jarbasai <jarbasai>
2017-07-23 20:19:28 +0100

    update

--- dev_setup.sh
+++ dev_setup.sh
@@ -53,7 +53,8 @@ pip install --upgrade virtualenv
 
 # copy global open-cv to virtual env, pip open-cv version doesnt work
 # https://medium.com/@manuganji/installation-of-opencv-numpy-scipy-inside-a-virtualenv-bf4d82220313
-sudo cp /usr/lib/pymodules/python2.7/cv* $VIRTUALENV_ROOT/lib/python2.7/site-packages/
+sudo cp /usr/lib/python2.7/dist-packages/cv*
+$VIRTUALENV_ROOT/lib/python2.7/site-packages/
 
 # tensorflow from binary
 pip2 install --upgrade https://storage.googleapis
