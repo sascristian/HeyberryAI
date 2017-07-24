a99ac494ed24243a2643941b5370f98a36b2254f
jarbasai <jarbasai>
2017-07-24 02:42:48 +0100

    update

--- build_host_setup_debian.sh
+++ build_host_setup_debian.sh
@@ -38,6 +38,6 @@ sudo apt-get install -y \
 
 # get geckodriver to usr/bin for browser service
 wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
-tar -xvzf geckodriver*
+tar -xvzf geckodriver-v0.18.0-linux64.tar.gz
 chmod +x geckodriver
 sudo mv geckodriver /usr/local/bin/
