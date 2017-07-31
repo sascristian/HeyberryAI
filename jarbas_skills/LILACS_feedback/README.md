cc9a58d84bc9b00d6e4b8cda4b9e653de88a206e
jarbasai <jarbasai>
2017-07-31 19:58:34 +0100

    more skills

--- jarbas_skills/LILACS_feedback/README.md
+++
@@ -1,5 +0,0 @@
-# LILACS feedback skill
-
-this skill gives feedback to lilacs core, it has intents for positive and negative feedback, this is passed to lilacs core in order to correct answers
-
-a sort of primitive reinforcement learning
---
+++ jarbas_skills/skill_dictation/__init__.py
@@ -0,0 +1,109 @@
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+from mycroft.skills.intent_service import IntentParser
+import os, time
+
+__author__ = 'jarbas'
+
+logger = getLogger(__name__)
+
+
+class DictationSkill(MycroftSkill):
+
+    def __init__(self):
+        super(DictationSkill, self).__init__()
+        self.dictating = False
+        self.parser = None
+        self.words = ""
+        self.dictation_name = None
+        self.path = "/home/user/jarbas-core/mycroft/skills/DictationSkill/dictations"
+        self.path = os.path.dirname(__file__) + "/dictations"
+        self.reload_skill = False
+        # check if folders exist
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+    def initialize(self):
+
+        start_parrot_intent = IntentBuilder("StartDictationIntent")\
+            .require("StartKeyword").require(
+            "DictationKeyword").optionally("DictationFileName").build()
+
+        self.register_intent(start_parrot_intent,
+                             self.handle_start_dictation_intent)
+
+        stop_parrot_intent = IntentBuilder("StopDictationIntent") \
+            .require("StopKeyword").require("DictationKeyword").build()
+
+        self.register_intent(stop_parrot_intent,
+                             self.handle_stop_dictation_intent)
+
+        self.parser = IntentParser(self.emitter)
+
+    def handle_start_dictation_intent(self, message):
+        name = message.data.get("DictationFileName")
+        if not name:
+            self.dictation_name = time.asctime()
+        else:
+            self.dictation_name = name
+        self.dictating = True
+        self.speak("Dictation Mode Started", expect_response=True)
+
+    def handle_stop_dictation_intent(self, message):
+        self.dictating = False
+        self.speak("Dictation Mode Stopped")
+        self.save()
+
+    def handle_read_last_dictation_intent(self, message):
+        self.speak_dialog("dictation")
+        self.speak(self.words)
+
+    def save(self):
+        # save
+        path = self.path + "/" + self.dictation_name + ".txt"
+        with open(path, "w") as f:
+            f.write(self.words)
+
+    def stop(self):
+        if self.dictating:
+            self.dictating = False
+            self.save()
+            self.speak("Dictation Mode Stopped")
+
+    def Converse(self, utterances, lang="en-us"):
+        if self.dictating:
+            intent, skill_id = self.parser.determine_intent(utterances[0])
+            if skill_id == self.skill_id:
+                return False
+            else:
+                self.words += (utterances[0]) + "\n"
+                self.speak("", expect_response=True)
+                return True
+        else:
+            return False
+
+
+def create_skill():
+    return DictationSkill()
+
