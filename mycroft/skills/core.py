d18757ef8dc98c3b5579ad3f7186b24482022192
jarbasai <jarbasai>
2017-08-04 21:57:31 +0100

    settings

--- mycroft/skills/core.py
+++ mycroft/skills/core.py
@@ -241,9 +241,9 @@ class MycroftSkill(object):
             return self._settings
         except:
             try:
-                self._settings = SkillSettings(join(self._dir, 'settings.json'))
+                self._settings = SkillSettings(self._dir)
             except:
-                self._settings = SkillSettings(join(dirname(__file__), 'settings.json'))
+                self._settings = SkillSettings(dirname(__file__))
             return self._settings
 
     def bind(self, emitter):
