e2e67a299d81f931f7117d2c26dff6dec39aadd2
jarbasai <jarbasai>
2017-08-13 21:21:56 +0100

    firefox package is different

---
+++ build_host_setup_ubuntu.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y \
+    git \
+    python \
+    python-dev \
+    python-setuptools \
+    python-virtualenv \
+    python-gobject-dev \
+    virtualenvwrapper \
+    libtool \
+    libffi-dev \
+    libssl-dev \
+    autoconf \
+    automake \
+    bison \
+    swig \
+    libglib2.0-dev \
+    s3cmd \
+    portaudio19-dev \
+    mpg123 \
+    screen \
+    flac \
+    curl \
+    libicu-dev \
+    pkg-config \
+    automake \
+    xvfb \
+    firefox \
+    vlc \
+    libopencv-dev \
+    python-opencv \
+    libxml2-dev \
+    libxslt1-dev \
+    build-essential \
+    gfortran \
+    libatlas-base-dev \
+    gnupg \
+    libcurl4-openssl-dev \
+    libjpeg-dev \
+    libfann-dev \
+    libgeos-dev
+
+# get geckodriver to usr/bin for browser service
+wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.18.0-linux64.tar.gz
+chmod +x geckodriver
+sudo mv geckodriver /usr/local/bin/
