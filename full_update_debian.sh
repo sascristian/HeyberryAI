6f6268a36ef75eb538ee79723f40398ca6d2eb8a
jarbasai <jarbasai>
2017-08-18 18:34:58 +0100

    full update

--- full_update_debian.sh
+++ full_update_debian.sh
@@ -1,5 +1,8 @@
 ./mycroft.sh stop
-git pull
+# WARNING overwrite all local changes
+git fetch origin patch-15
+git reset --hard FETCH_HEAD
+git clean -df
 ./build_host_setup_debian.sh
 ./update-dev.sh
 ./mycroft.sh start
