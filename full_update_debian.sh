62b80db9751359268e026f5a4c923881e1ce9d4f
jarbasai <jarbasai>
2017-08-18 18:37:58 +0100

    hard reset git

--- full_update_debian.sh
+++ full_update_debian.sh
@@ -2,7 +2,8 @@
 # WARNING overwrite all local changes
 git fetch origin patch-15
 git reset --hard FETCH_HEAD
-git clean -df
+# Clean all files not in repo, will erase keys and all data in this folder
+# git clean -df
 ./build_host_setup_debian.sh
 ./update-dev.sh
 ./mycroft.sh start
