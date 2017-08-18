fcc6504880156aedbaa89cf9db29e26b3f70cd70
jarbasai <jarbasai>
2017-08-18 20:36:48 +0100

    bug hunt

--- mycroft/skills/core.py
+++ mycroft/skills/core.py
@@ -262,6 +262,8 @@ class MycroftSkill(object):
             self.__register_stop()
             self.emitter.on('enable_intent', self.handle_enable_intent)
             self.emitter.on('disable_intent', self.handle_disable_intent)
+            self.emitter.on('intent_failure',
+                            self.handle_update_message_context)
 
     def __register_stop(self):
         self.stop_time = time.time()
@@ -530,7 +532,6 @@ class FallbackSkill(MycroftSkill):
 
         #  list of fallback handlers registered by this instance
         self.instance_fallback_handlers = []
-        self.emitter.on('intent_failure', self.handle_update_message_context)
 
     @classmethod
     def make_intent_failure_handler(cls, ws):
