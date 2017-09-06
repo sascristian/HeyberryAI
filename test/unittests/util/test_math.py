b67089b542caa86159fc26c9c01970a178cc9ae5
jarbasai <jarbasai>
2017-09-06 02:04:36 +0100

    math

--- test/unittests/util/test_math.py
+++ test/unittests/util/test_math.py
@@ -20,6 +20,10 @@ class TestMathExtractFormat(unittest.TestCase):
             [['10', '!', 'next']])
 
         self.assertEqual(
+            extract_expression("square root of 4"),
+            [['4.0', 'sqrt', 'next']])
+
+        self.assertEqual(
             extract_expression("one plus pi plus x"),
             [['1', '+', 'pi'], ['prev', '+', 'x']])
 
@@ -38,23 +42,92 @@ class TestMathExtractFormat(unittest.TestCase):
     def test_solve_exps(self):
         self.assertEqual(
             solve_expression("one dog plus one dog plus two frogs"),
-            "1dog + 1dog + 2frogs")
+            "2dog + 2frogs")
+
+        self.assertEqual(
+            solve_expression("one dog plus one dog plus one plus two "
+                             "frogs"),
+            "2dog + 1 + 2frogs")
+
+        self.assertEqual(
+            solve_expression("one dog divided by one dog plus two frogs"),
+            "1 + 2frogs")
+
+        self.assertEqual(
+            solve_expression("evil divided by evil plus two frogs"),
+            "1 + 2frogs")
+
+        self.assertEqual(
+            solve_expression("one dog minus one dog plus 1"),
+            "1")
+
+        self.assertEqual(
+            solve_expression("one dog minus one dog"),
+            "")
+
+        self.assertEqual(
+            solve_expression(
+                "one dog plus one dog plus one dog plus two frogs"),
+            "3dog + 2frogs")
+
+        # TODO
+        # self.assertEqual(
+        #    solve_expression("one dog minus one dog plus one cat"),
+        #    "1cat")
+
+        # self.assertEqual(
+        #    solve_expression("one dog multiplied by one dog plus two frogs"),
+        #    "1dog squared + 2frogs")
+
+        # self.assertEqual(
+        #    solve_expression("one dog minus + one dog plus two frogs"),
+        #    "2frogs")
+
+        #      self.assertEqual(
+        #           solve_expression("one dog minus one dog plus two frogs"),
+        #            "2frogs")
+
+
+        # TODO 3dog + 1 cat
+        self.assertEqual(
+            solve_expression("one dog plus one cat plus two dog"),
+            "1dog + 1cat + 2dog")
 
         self.assertEqual(
             solve_expression("one plus two plus one"),
             '4')
 
         self.assertEqual(
+            solve_expression("two squared"),
+            '4')
+
+        self.assertEqual(
+            solve_expression("3 cubed"),
+            '27')
+
+        self.assertEqual(
+            solve_expression("square root of four"),
+            '2')
+
+        #  self.assertEqual(
+        #      solve_expression("x equals one plus two"),
+        #      '3')#TODO = operation
+
+        self.assertEqual(
             solve_expression("ten factorial"),
             '3628800')
 
         self.assertEqual(
-            solve_expression("one plus pi plus x"),
+            solve_expression("one plus pi plus x", nice=False),
             "4.14159265359 + x")
 
         self.assertEqual(
-            solve_expression("y divided by x"),
-            "y / x")
+            solve_expression("one plus pi plus x", nice=True),
+            "4 and a seventh + x")
+
+        self.assertEqual(
+            solve_expression("dog divided by frog"),
+            "dog / frog")
 
         self.assertEqual(
             solve_expression("one times seven plus two multiply by two"),
