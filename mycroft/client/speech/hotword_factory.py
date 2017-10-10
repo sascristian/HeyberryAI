203ae95161fbedce86d9befea9d362d961ed81d0
Jarbas <jarbasai@mailfence.com>
2017-10-10 02:03:29 +0100

    add julius

--- mycroft/client/speech/hotword_factory.py
+++ mycroft/client/speech/hotword_factory.py
@@ -1,25 +1,32 @@
-# Copyright 2017 Mycroft AI Inc.
+# Copyright 2017 Mycroft AI, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This file is part of Mycroft Core.
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
 import tempfile
 import time
+import re
 
 import os
 from os.path import dirname, exists, join, abspath
 
-from mycroft.configuration import ConfigurationManager
+from mycroft.configuration import Configuration
 from mycroft.util.log import LOG
+from subprocess import Popen, PIPE, STDOUT
+
+__author__ = 'seanfitz, jdorleans, jarbas'
 
 
 RECOGNIZER_DIR = join(abspath(dirname(__file__)), "recognizer")
@@ -30,14 +37,18 @@ class HotWordEngine(object):
         self.lang = str(lang).lower()
         self.key_phrase = str(key_phrase).lower()
         # rough estimate 1 phoneme per 2 chars
-        self.num_phonemes = len(key_phrase) / 2 + 1
+        self.num_phonemes = len(key_phrase)
         if config is None:
-            config = ConfigurationManager.get().get("hot_words", {})
+            config = Configuration.get().get("hot_words", {})
             config = config.get(self.key_phrase, {})
         self.config = config
-        self.listener_config = ConfigurationManager.get().get("listener", {})
+        self.listener_config = Configuration.get().get("listener", {})
+        self.need_wav = False
 
     def found_wake_word(self, frame_data):
+        return False
+
+    def found_wake_word_from_wav(self, wav_file):
         return False
 
 
@@ -101,7 +112,11 @@ class SnowboyHotWord(HotWordEngine):
     def __init__(self, key_phrase="hey mycroft", config=None, lang="en-us"):
         super(SnowboyHotWord, self).__init__(key_phrase, config, lang)
         # Hotword module imports
-        from snowboydecoder import HotwordDetector
+        try:
+            from snowboydecoder import HotwordDetector
+        except ImportError:
+            from mycroft.client.speech.recognizer.snowboy.snowboydecoder \
+                import HotwordDetector
         # Hotword module config
         module = self.config.get("module")
         if module != "snowboy":
@@ -111,30 +126,99 @@ class SnowboyHotWord(HotWordEngine):
         models = self.config.get("models", {})
         paths = []
         for key in models:
-            paths.append(models[key])
+            path = models[key]
+            if ".pmdl" not in path and ".umdl" not in path:
+                path += ".pmdl"
+            if "/" not in path:
+                path = dirname(
+                    __file__) + "/recognizer/snowboy/resources/" + path
+            paths.append(path)
         sensitivity = self.config.get("sensitivity", 0.5)
         self.snowboy = HotwordDetector(paths,
                                        sensitivity=[sensitivity] * len(paths))
-        self.lang = str(lang).lower()
-        self.key_phrase = str(key_phrase).lower()
 
     def found_wake_word(self, frame_data):
         wake_word = self.snowboy.detector.RunDetection(frame_data)
         return wake_word == 1
 
 
+class JuliusHotWord(HotWordEngine):
+    def __init__(self, key_phrase="hey mycroft", config=None, lang="en-us"):
+        super(JuliusHotWord, self).__init__(key_phrase, config, lang)
+        self.need_wav = True
+        # Hotword module config
+        module = self.config.get("module")
+        if module != "julius":
+            LOG.warning(
+                str(module) + " module does not match with "
+                              "Hotword class julius")
+        # Hotword module params
+        self.jconf = RECOGNIZER_DIR + "/julius/julius.jconf"
+        self.grammar = RECOGNIZER_DIR + "/julius/julius"
+        self.min_score = 0.5  # TODO find good compromise
+        self.debug = True
+
+    def ask_julius(self, file):
+        path = RECOGNIZER_DIR + "/julius/julius.txt"
+        with open(path, "w") as f:
+            f.write(file)
+        hyp = None
+        score = -1.0
+        args = ["julius", '-C', self.jconf, '-gram', self.grammar,
+                '-input',
+                "rawfile", "-filelist", path]
+        p = Popen(args,
+                  stdout=PIPE,
+                  stderr=STDOUT)
+
+        while True:
+            line = p.stdout.readline()
+            if not line:
+                continue
+            if self.debug and "warning" in line.lower():
+                LOG.error(line)
+            if "error" in line.lower() or "search failed" in line.lower():
+                if self.debug:
+                    LOG.error(line)
+                break
+            m = re.match(r'^(sentence|cmscore)1: (.*)', line)
+            if m:
+                attr = m.group(1)
+                val = m.group(2)
+                if attr == 'sentence':
+                    hyp = val.split()
+                elif attr == 'cmscore':
+                    score = [float(v) for v in val.split()]
+                break
+
+        if score > self.min_score:
+            return hyp
+        return None
+
+    def found_wake_word(self, frame_data):
+        # TODO frame_data to .wav
+        output = RECOGNIZER_DIR + "/julius/julius.wav"
+        return False
+
+    def found_wake_word_from_wav(self, wav_file):
+        hyp = self.ask_julius(wav_file)
+        return hyp and self.key_phrase in hyp.lower()
+
+
 class HotWordFactory(object):
     CLASSES = {
         "pocketsphinx": PocketsphinxHotWord,
-        "snowboy": SnowboyHotWord
+        "snowboy": SnowboyHotWord,
+        "julius": JuliusHotWord
     }
 
     @staticmethod
     def create_hotword(hotword="hey mycroft", config=None, lang="en-us"):
         LOG.info("creating " + hotword)
         if not config:
-            config = ConfigurationManager.get().get("hotwords", {})
-        module = config.get(hotword).get("module", "pocketsphinx")
+            config = Configuration.get().get("hotwords", {})
+        module = config.get(hotword, {}).get("module", "pocketsphinx")
         config = config.get(hotword, {"module": module})
         clazz = HotWordFactory.CLASSES.get(module)
         return clazz(hotword, config, lang=lang)
+
